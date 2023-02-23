8c5d30345e3d520e0b4e41712a63f363694178fb
Lucas Lucovas <lucaslucovas@gmail.com>
2023-02-23 19:54:45 -0300

    cambios en el tp

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
-let yourAge = parseInt(prompt('Ingrese su edad'))
-let peopleInTheQueue = parseInt(prompt('Ingrese cuanta gente hay en la cola'))
-let timeInQueue = 0;
-function queue(number){
-    for (let i = 0; i < number; i++) {
-        timeInQueue += 2
-    }
-    return timeInQueue
+let notaPrimerTrimestre = parseInt(prompt('Ingrese su nota del primer trimestre'))
+let notaSegundoTrimestre = parseInt(prompt('Ingrese su nota del segundo trimestre'))
+let notaTercerTrimestre= parseInt(prompt('Ingrese su nota del tercer trimestre'))
+
+let TodasLasNotas = {
+    notaPrimerTrimestre,
+    notaSegundoTrimestre,
+    notaTercerTrimestre,
 }
 
-
-
-if (timeInQueue == 10) {
-    alert('10 min de cola restantes')
-}else if (timeInQueue == 20) {
-    alert('20 min de cola restantes')
-}else if (timeInQueue == 30) {
-    alert('30 min de cola restantes')
-}else if (timeInQueue == 40) {
-    alert('40 min de cola restantes')
-}else if (timeInQueue == 50) {
-    alert('50 min de cola restantes')
-}else if (timeInQueue == 60) {
-    alert('1Hs restante de cola')
+let promedio = 0;
+for (let key in TodasLasNotas) {
+  promedio += TodasLasNotas[key];
 }
 
-queue(peopleInTheQueue)
-
-
-
-
-
-
-
+promedio = promedio / 3
+if (promedio >= 6) {
+    alert(`Aprobaste tu nota es: ${Math.round(promedio)}`)
+}else{
+    alert(`Reprobaste tu nota es: ${Math.round(promedio)}`)
+}
 
 
 
